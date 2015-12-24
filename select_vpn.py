d775d7355fe14ba4abc86a034bb93cf03d5084ef
Adam Duston <aduston@slac.stanford.edu>
2015-12-23 17:05:31 -0800

    Open and close file in separate blocks. Trying to deal with the occasional issue where the json file will end up with extra } characters after updating.

--- select_vpn.py
+++ select_vpn.py
@@ -128,10 +128,13 @@ def update_transmission_config(config_file, ip):
     :return:
     """
 
-    with open(config_file, 'r+') as settings_json:
+    with open(config_file, 'r') as settings_json:
         data = json.load(settings_json)
+
+    if data:
         data['bind-address-ipv4'] = str(ip)
-        settings_json.seek(0)
+
+    with open(config_file, 'w') as settings_json:
         json.dump(data, settings_json, indent=4)
 
 
